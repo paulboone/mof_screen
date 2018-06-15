30f7cab14527467f58a4c5d28ef1c1a0f30a68af
Paul Boone <narcissus.pong@gmail.com>
2018-06-15 12:42:05 -0400

    do N copies of each MOF

--- generate_configs.sh
+++ generate_configs.sh
@@ -12,24 +12,27 @@
 
 set -e
 
+num_per_mof=$1
+
 while IFS='$\n' read -r line; do
   mof=${line% *}
   num=${line#* }
+  for i in `seq 1 ${num_per_mof}`; do
+    mkdir -p $mof.$i
+    cd $mof.$i
+    if [ -e ../../core-mof-1.0-ddec/${mof}.cif ]; then
+      echo "${mof}.$i"
+      cp ../../core-mof-1.0-ddec/${mof}.cif ./$mof.cif
+    else
+      echo "${mof}.${i} (using $(cd ../../core-mof-1.0-ddec/ && ls -1 ${mof}_*.cif))"
+      cp ../../core-mof-1.0-ddec/${mof}_*.cif ./$mof.cif
+    fi;
 
-  mkdir -p $mof
-  cd $mof
-  if [ -e ../../core-mof-1.0-ddec/${mof}.cif ]; then
-    echo "${mof}.cif"
-    cp ../../core-mof-1.0-ddec/${mof}.cif ./$mof.cif
-  else
-    echo "$(cd ../../core-mof-1.0-ddec/ && ls -1 ${mof}_*.cif)"
-    cp ../../core-mof-1.0-ddec/${mof}_*.cif ./$mof.cif
-  fi;
+    gen_mof_flex_ff_rigid_molecule_lammps_config ./$mof.cif CO2 -n $num > gen_config.log
+    mv gen_config.log ./tmp
+    cp ../../mof_screen_co2.lammps ./ && bash modify_lammps.sh ./mof_screen_co2.lammps
+    sed -e "s|^#SBATCH --job-name=*.*$|#SBATCH --job-name=$mof.$i|" ../../screen.slurm > screen.slurm
 
-  gen_mof_flex_ff_rigid_molecule_lammps_config ./$mof.cif CO2 -n $num > gen_config.log
-  mv gen_config.log ./tmp
-  cp ../../mof_screen_co2.lammps ./ && bash modify_lammps.sh ./mof_screen_co2.lammps
-  sed -e "s|^#SBATCH --job-name=*.*$|#SBATCH --job-name=$mof|" ../../screen.slurm > screen.slurm
-
-  cd ..
+    cd ..
+  done
 done
